429cb1d5bf5c78955ce90197229cf364a5a90111
Jayani Athukorala <sandunkauwu@gmail.com>
2026-03-09 18:14:56 +0100

    Add temp IO class

---
+++ src/main/java/se/lexicon/IO.java
@@ -0,0 +1,12 @@
+package se.lexicon;
+
+//To be removed. This is to downgrade the JDK version for deployment
+public class IO {
+    public static void print(String message) {
+        System.out.println(message);
+    }
+
+    public static void println(String message) {
+        System.out.println(message);
+    }
+}
